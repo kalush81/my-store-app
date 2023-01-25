2fb079f2bd316ed7825aecef2c679def14febbc0
krzysiek <krzysiek@Krzysztofs-MacBook-Pro-2.local>
2023-01-25 15:21:28 +0100

    Add Auth to keystone UI

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,22 +1,43 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
 
 const databaseUrl =
   process.env.DATABASE_URL || 'mongodb://localhost/my-store-cluster';
 
 const sessionConfig = { age: 3600, secret: process.env.COOKIE_SECRET };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseUrl,
-  },
-  lists: createSchema({}),
-  ui: { isAccessAllowed: () => true },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseUrl,
+    },
+    lists: createSchema({
+      User,
+    }),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    ui: { isAccessAllowed: ({ session }) => !!session?.data },
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
