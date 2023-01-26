3f8f8bada83a1b3c3e6a1ca79e0a9b343ef140f1
krzysiek <krzysiek@Krzysztofs-MacBook-Pro-2.local>
2023-01-26 16:18:28 +0100

    Create relation between Image and Product, Update UI

--- backend/schemas/Product.ts
+++ backend/schemas/Product.ts
@@ -1,4 +1,4 @@
-import { integer, select, text } from '@keystone-next/fields';
+import { integer, relationship, select, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 
 export const Product = list({
@@ -14,5 +14,14 @@ export const Product = list({
       ui: { displayMode: 'segmented-control' },
     }),
     price: integer(),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
   },
 });
